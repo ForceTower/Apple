572145e50dfd3a136f8519f5db76cd24d6c467e3
João Sena <joaopaulo761@gmail.com>
2023-07-16 18:11:28 -0300

    Add firebase

--- UNES/SceneDelegate.swift
+++ UNES/SceneDelegate.swift
@@ -12,7 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        // windowScene.coordinateSpace.bounds
         window = UIWindow(frame:  windowScene.coordinateSpace.bounds)
         
         let coordinator = MainCoordinator(container: AppDIContainer(), window: window!)
@@ -50,7 +49,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        // (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
